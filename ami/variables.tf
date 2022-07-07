fdec1435d35835c4d8632aa36282c901d58668b7
Evandro Pomatti <evandro.pomatti@gmail.com>
2022-07-06 23:20:38 -0300

    vars

--- ami/variables.tf
+++ ami/variables.tf
@@ -8,21 +8,6 @@ variable "project_name" {
   type    = string
 }
 
-variable "availability_zone_1" {
-  default = "sa-east-1a"
-  type    = string
-}
-
-variable "availability_zone_2" {
-  default = "sa-east-1b"
-  type    = string
-}
-
-variable "availability_zone_3" {
-  default = "sa-east-1c"
-  type    = string
-}
-
 variable "allowed_origin" {
   default     = "0.0.0.0/0"
   type        = string
